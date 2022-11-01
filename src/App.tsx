fefb6e323638962e42eb268dd8226c6d92ff34e9
Bence Webber <thawildcards@gmail.com>
2022-11-01 21:02:02 +0000

    Create animated title

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
+import AnimatedTitle from './components/AnimatedTitle';
+
+
 function App() {
+  const [showSecond, setShowSecond] = useState(false)
+
   return (
-    <div className="text-5xl">
-      Hi
+    <div className="flex justify-center items-center h-screen">
+      <div className="flex flex-col w-[25rem] h-[25rem]">
+        <AnimatedTitle 
+          text="Hi,"
+          delay={1000}
+          after={() => {setShowSecond(true)}}
+          className="text-[5rem] font-semibold" 
+        />
+        {showSecond ?
+          <AnimatedTitle 
+            text="I'm Bence"
+            delay={1000}
+            after={() => {console.log("DONE")}}
+            className="text-[5rem] font-semibold" 
+          /> : null
+        }
+      </div>
     </div>
   );
 }
---
+++ src/components/AnimatedTitle.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
+
+
+type Props = {
+  text: string,
+  delay?: number,
+  after: Function,
+  className: string
+}
+
+const AnimatedTitle: React.FC<Props> = ({ text, delay, after, className }) => {
+  const [content, setContent] = useState("");
+
+  const typeKeywordAnimation = (newText: string, after: Function, index: number = 0) => {
+    index++
+    setContent(newText.slice(0, index));
+    if (index <= newText.length) {
+      setTimeout(() => {typeKeywordAnimation(newText, after, index)}, 75); 
+    } else {
+      after();
+    }
+  }
+
+  useEffect(() => {
+    setTimeout(() => {typeKeywordAnimation(text, after)}, delay);
+  }, [])
+
+  return (
+    <div className={className}>
+      <span className="text-primary">{content}</span>
+    </div>
+  )
+}
+
+export default AnimatedTitle
