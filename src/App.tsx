5b42a909915559a01846fcd6cd12f98f7e04d440
Bence Webber <thawildcards@gmail.com>
2022-11-03 11:38:18 +0000

    Remove animated title

--- src/App.tsx
+++ src/App.tsx
@@ -8,20 +8,17 @@ function App() {
   const [animationStage, setAnimationStage] = useState(2)
 
   return (
-    <div className="flex justify-center items-center h-screen">
+    <div className="flex justify-center items-center h-screen subpixel-antialiased">
       <div className="flex flex-col items-start w-[40rem] h-[25rem]">
           <div className="text-3xl font-semibold">
             Hi, I'm
           </div>
 
-          <AnimatedTitle 
-            text="Bence Webber"
-            delay={0}
-            after={() => {}}
-            className="text-[5rem] font-semibold subpixel-antialiased" 
-          />
+          <div className="text-[5rem] font-semibold">
+            Bence Webber
+          </div>
 
-          <button className="transition-colors border border-white border-opacity-60 hover:border-opacity-100 rounded-md text-2xl px-6 py-2">
+          <button className="transition-colors border border-white border-opacity-60 hover:border-opacity-100 rounded-lg text-2xl px-6 py-2">
             Portfolio
           </button>
       </div>
