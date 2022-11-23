fddda1b3bcd96fb2d056f38617a618008ad97d25
Bence Webber <thawildcards@gmail.com>
2022-11-23 18:22:12 +0000

    Set up React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,18 @@
-import React, { useState } from 'react';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import AnimatedTitle from './components/AnimatedTitle';
+import Home from "./pages/Home";
+import About from "./pages/About";
 
 
 function App() {
-  const [animationStage, setAnimationStage] = useState(2)
-
   return (
-    <div className="flex justify-center items-center h-screen subpixel-antialiased">
-      <div className="flex flex-col items-start w-[40rem] h-[25rem]">
-          <div className="text-3xl font-semibold">
-            Hi, I'm
-          </div>
-
-          <div className="text-[5rem] font-semibold">
-            Bence Webber
-          </div>
-
-          <button 
-            onClick={() => { window.open('https://github.com/bencerwn?tab=repositories', '_blank'); }}
-            className="transition-colors border border-white border-opacity-60 hover:border-opacity-100 rounded-lg text-2xl px-6 py-2"
-          >
-            Projects
-          </button>
-      </div>
+    <div className="h-screen">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,8 +6,23 @@ type Props = {
 
 const Home: React.FC<Props> = ({ }) => {
   return (
-    <div>
-      Home
+    <div className="flex justify-center items-center h-full subpixel-antialiased">
+      <div className="flex flex-col items-start w-[40rem] h-[25rem]">
+        <div className="text-3xl font-semibold">
+          Hi, I'm
+        </div>
+
+        <div className="text-[5rem] font-semibold">
+          Bence Webber
+        </div>
+
+        <button 
+          onClick={() => { window.open('https://github.com/bencerwn?tab=repositories', '_blank'); }}
+          className="transition-colors border border-white border-opacity-60 hover:border-opacity-100 rounded-lg text-2xl px-6 py-2"
+        >
+          Projects
+        </button>
+      </div>
     </div>
   )
 }
