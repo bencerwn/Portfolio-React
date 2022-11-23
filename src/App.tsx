8e20e7ff0639e6abb916c45b49f296fca02e12f7
Bence Webber <thawildcards@gmail.com>
2022-11-23 18:59:12 +0000

    Add Navbar to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import About from "./pages/About";
 
 
 function App() {
   return (
-    <div className="h-screen">
+    <div className="flex flex-col text-center font-medium subpixel-antialiased h-screen">
+      <Navbar />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
